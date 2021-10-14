d6b4e420bfd930ddef0e1dd429ec4be083a7c5ac
Jean-Noël Charon <jncharon@capcod.eu>
2021-10-14 11:07:58 +0200

    deploy update to use the profile by default

--- deploy_s3.cmd
+++ deploy_s3.cmd
@@ -1,5 +1,5 @@
-aws s3 sync . s3://www.charon.org/sql/ --delete --acl public-read --exclude "git"
-aws s3 rm s3://www.charon.org/sql/.git
-aws s3 rm s3://www.charon.org/sql/deploy_s3.cmd
+aws s3 sync . s3://www.charon.org/sql/ --delete --acl public-read --exclude "git" --profile charon
+aws s3 rm s3://www.charon.org/sql/.git --profile charon
+aws s3 rm s3://www.charon.org/sql/deploy_s3.cmd --profile charon
 
-aws cloudfront create-invalidation --distribution-id E2LH0VP7P26H2B --paths "/sql"
+aws cloudfront create-invalidation --distribution-id E2LH0VP7P26H2B --paths "/sql" --profile charon
